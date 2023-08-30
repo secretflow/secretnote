268dffe4964adb3d30ebd07d26ad50fd663269da
Tony Wu <tonywu6@protonmail.com>
2023-08-30 15:47:48 +0800

    chore(devcontainer): add demo compose config

---
+++ .devcontainer/standalone/home/scripts/install-workspace.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+python3 -m pip install --force-reinstall ~/workspace
---
+++ .devcontainer/standalone/home/scripts/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# PATH is currently a mess
+source ~/.bashrc
+
+ray start \
+  --head \
+  --port=6379 \
+  --dashboard-host "0.0.0.0" \
+  --resources="{\"$SELF_PARTY\": 4}"
+
+jupyter lab --ip "0.0.0.0" --no-browser
