8a9a23085a1b94788a76ea835148596ab9a9883a
Tony Wu <tonywu6@protonmail.com>
2023-09-25 19:59:49 +0800

    fix: adjust Python version

--- pyprojects/secretnote/tests/secretnote/compat/secretflow/device/test_driver.py
+++ pyprojects/secretnote/tests/secretnote/compat/secretflow/device/test_driver.py
@@ -3,4 +3,7 @@ def test_cluster_party_export_omit_none():
     from secretnote.compat.secretflow.device.driver import SFClusterParty
 
     party = SFClusterParty(address="localhost:7860")
-    assert party.dict() == {"address": "localhost:7860"}
+    assert party.dict() == {
+        "listen_addr": "localhost:7860",
+        "address": "localhost:7860",
+    }
