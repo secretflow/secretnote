5101b52df974c3064713802f8bc4c8807b65dae1
newbyvector <vectorse@126.com>
2024-03-27 10:43:14 +0800

    fix: support lsp in docker

---
+++ docker/app/root/.jupyter/jupyter_server_config.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from traitlets.config import get_config
+
+c = get_config()
+
+c.ServerApp.allow_origin = "*"
+
+c.ServerApp.allow_remote_access = True
+
+c.ServerApp.disable_check_xsrf = True
+
+c.ServerApp.token = ""
+
+c.ServerApp.ip = "*"
+
+c.ServerApp.root_dir = "/root/workspace"
+
+c.LanguageServerManager.autodetect = False
+
+c.LanguageServerManager.language_servers = {
+    "ruff-lsp": {
+        # if installed as a binary
+        "argv": [
+            "ruff-lsp",
+        ],
+        "languages": ["python"],
+        "version": 2,
+        "mime_types": ["text/x-python"],
+        "display_name": "ruff-lsp",
+    },
+    "libro-analyzer": {
+        # if installed as a binary
+        "argv": [
+            "node",
+            "node_modules/@difizen/libro-analyzer/index.js",
+            "--stdio",
+        ],
+        "languages": ["python"],
+        "version": 2,
+        "mime_types": ["text/x-python"],
+        "display_name": "libro-analyzer",
+    },
+}
