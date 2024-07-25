ccbc18e07a61c5ac446f038d0d4371579ffb8fe1
newbyvector <vectorse@126.com>
2024-07-25 14:31:33 +0800

    fix: update backend url

--- packages/secretnote-lite/src/utils/request.ts
+++ packages/secretnote-lite/src/utils/request.ts
@@ -1,12 +1,7 @@
 import { URL as LibroURL } from '@difizen/libro-jupyter';
 
 export const getRemoteBaseUrl = (targetId = '', endSlash = false) => {
-  return (
-    process.env.SECRETNOTE_BACKEND_URL +
-    '/secretnote/' +
-    targetId +
-    `${endSlash ? '/' : ''}`
-  );
+  return window.location.host + '/secretnote/' + targetId + `${endSlash ? '/' : ''}`;
 };
 
 export const getRemoteWsUrl = (targetId = '', endSlash = false) => {
