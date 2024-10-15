30fa48b2c7998c5f6f72426122fcfad8c1b198ae
z0gSh1u <zx.cs@qq.com>
2024-10-15 13:50:30 +0800

    fix: defaults mode to sf

--- pyprojects/secretnote/secretnote/__main__.py
+++ pyprojects/secretnote/secretnote/__main__.py
@@ -3,20 +3,19 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "mode", type=str, default='sf', help="Mode of SecretNote to start, in [`sf`, `scql`]. Default: `sf`."
+        "mode", type=str, default="sf", help="Mode of SecretNote to start, in [`sf`, `scql`]. Default: `sf`."
     )
     args, rest_args = parser.parse_known_args()
 
-    if args.mode == "sf":
-        from .sf.server.app import SecretNoteApp
-
-        SecretNoteApp().launch(rest_args)
-    elif args.mode == "scql":
+    if args.mode == "scql":
         from .scql.server.app import SecretNoteApp
 
         SecretNoteApp().launch(rest_args)
     else:
-        raise ValueError(f"Invalid mode argument: {args.mode}")
+        # mode == "sf" or anyhow, defaults to sf mode.
+        from .sf.server.app import SecretNoteApp
+
+        SecretNoteApp().launch(rest_args)
 
 
 if __name__ == "__main__":
